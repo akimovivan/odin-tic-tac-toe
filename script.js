60bb459691c687d6a9f667941a2ebda0ee08df1f
Akimov Ivan <akimovivan13@gmail.com>
2025-08-13 03:38:50 +0300

    Finish up

--- script.js
+++ script.js
@@ -2,78 +2,98 @@
 
 const gameObject = function () {
         let fieldObj;
-        let playingField = [
-                [" ", " ", " "],
-                [" ", " ", " "],
-                [" ", " ", " "],
-        ];
+        let playingField = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+        let players = {
+                "x": { name: "player1", score: 0, element: document.getElementById("scoreX") },
+                "o": { name: "player2", score: 0, element: document.getElementById("scoreY") },
+        };
 
         let currentMove = "x";
+        let startingMove = "x";
 
+        const tieElement = document.getElementById("scoreTie");
+        let tieScore = 0;
+
+        const turnXElement = document.getElementById("turnX");
+        const turnOElement = document.getElementById("turnO");
+
+        const changeActiveMove = () => {
+                turnXElement.classList.toggle("active");
+                turnOElement.classList.toggle("active");
+                currentMove = (currentMove == "x") ? "o" : "x";
+        }
         const checkGameState = () => {
-                for (let i = 0; i < 3; i++) {
-                        for (let j = 0; j < 3; j++) {
-                                if (playingField[i][j] == " ") {
-                                        return true;
-                                }
+                for (let i = 0; i < 9; i++) {
+                        if (playingField[i] == " ") {
+                                return true;
                         }
                 }
                 return false;
         }
 
         const move = (id) => {
-                const { i, j } = translateIdToIndexes(id);
-                if (playingField[i][j] != " ") {
-                        return "illegal move";
+                if (playingField[id] != " ") {
+                        alert("Illegal move");
+                        return;
                 }
 
-                playingField[i][j] = currentMove;
-                currentMove = (currentMove == "x") ? "o" : "x";
+                playingField[id] = currentMove;
+                changeActiveMove();
 
-                let victor = checkVictory();
+                const victor = checkVictory();
+                const isDraw = checkGameState();
 
                 if (victor != " ") {
-                        return victor;
+                        players[victor].score++;
+                        players[victor].element.innerText = players[victor].score;
+                        clearPlayingField();
+                        return;
                 };
 
-                if (!checkGameState()) {
-                        return "draw";
+                if (!isDraw) {
+                        tieElement.innerText = ++tieScore;
+                        clearPlayingField();
+                        return;
                 }
 
-                renderField()
+                renderField();
         };
 
         const checkVictory = () => {
+                // Check rows
                 for (let i = 0; i < 3; i++) {
-                        let checkValue = playingField[i][0];
-                        for (let j = 0; j < 3; j++) {
-                                if (checkValue != playingField[i][j]) {
-                                        break;
-                                }
-                                if (j == 2 && checkValue != " ") return checkValue;
-                        }
+                        if (
+                                playingField[i * 3] != " " &&
+                                playingField[i * 3] == playingField[i * 3 + 1] &&
+                                playingField[i * 3 + 1] == playingField[i * 3 + 2]
+                        )
+                                return playingField[i * 3];
                 }
 
-                for (let j = 0; j < 3; j++) {
-                        let checkValue = playingField[0][j];
-                        for (let i = 0; i < 3; i++) {
-                                if (checkValue != playingField[i][j]) {
-                                        break;
-                                }
-                                if (i == 2 && checkValue != " ") return checkValue;
-                        }
+                // Check columns
+                for (let i = 0; i < 3; i++) {
+                        if (
+                                playingField[i] != " " &&
+                                playingField[i] == playingField[3 + i] &&
+                                playingField[3 + i] == playingField[6 + i]
+                        )
+                                return playingField[i];
                 }
 
-                if (playingField[0][0] != " " &&
-                        playingField[0][0] == playingField[1][1] &&
-                        playingField[1][1] == playingField[2][2]) {
-                        return playingField[0][0];
+                if (
+                        playingField[0] != " " &&
+                        playingField[0] == playingField[4] &&
+                        playingField[4] == playingField[8]
+                ) {
+                        return playingField[0];
                 }
 
-                if (playingField[2][0] != " " &&
-                        playingField[2][0] == playingField[1][1] &&
-                        playingField[1][1] == playingField[0][2]) {
-                        return playingField[2][0];
+                if (
+                        playingField[2] != " " &&
+                        playingField[2] == playingField[4] &&
+                        playingField[4] == playingField[6]
+                ) {
+                        return playingField[2];
                 }
 
                 return " ";
@@ -92,37 +112,59 @@ const gameObject = function () {
                 const children = fieldObj.children;
                 for (const child of children) {
                         const childId = child.dataset.id;
-                        let { i, j } = translateIdToIndexes(childId);
-                        child.innerText = playingField[i][j];
+                        child.innerText = playingField[childId];
                 }
         }
-        const translateIdToIndexes = (id) => {
-                const i = Math.floor(id / 3);
-                const j = id - 3 * i;
-                return { i, j }
+
+        const setPlayers = (player1, player2) => {
+                players["x"].name = player1;
+                players["o"].name = player2;
+                playerScoreX.innerText = player1;
+                playerScoreY.innerText = player2;
         }
 
-        const printField = () => { console.table(playingField) };
-        return { move, checkVictory, renderField, createField, printField };
+
+        const getPlayers = () => { return players; }
+
+        const clearPlayingField = () => {
+                currentMove = (startingMove == "x") ? "o" : "x";
+                startingMove = currentMove;
+                if (currentMove == "x") {
+                        turnXElement.classList.add("active");
+                        turnOElement.classList.remove("active");
+                } else {
+                        turnOElement.classList.add("active");
+                        turnXElement.classList.remove("active");
+                }
+                for (let i = 0; i < 9; i++) {
+                        playingField[i] = " ";
+                }
+                renderField();
+        }
+
+        return {
+                move,
+                createField,
+                setPlayers,
+        };
 }()
 
 window.addEventListener('DOMContentLoaded', function () {
         const fieldObj = document.getElementById("field");
         gameObject.createField(fieldObj);
 
+        const userForm = document.querySelector("form");
+        userForm.addEventListener("submit", (e) => {
+                e.preventDefault();
+                const myFormData = new FormData(e.target);
+                const formDataObj = Object.fromEntries(myFormData.entries());
+                gameObject.setPlayers(formDataObj.player1, formDataObj.player2);
+        })
+
         fieldObj.addEventListener("click", (e) => {
                 if (e.target.dataset.id === "undefined") {
                         console.error("Help me");
                 }
-                const state = gameObject.move(e.target.dataset.id);
-
-                if (state == "illegal move") {
-                        alert("illegal move");
-                } else if (state == "draw") {
-                        alert("draw");
-                } else if (state == "x" || state == "y") {
-                        alert(`${state} has won`);
-                        console.log("sas");
-                }
+                gameObject.move(e.target.dataset.id);
         })
 });
